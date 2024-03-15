92d1e60ed9b632e5f64a659dba08c13bceb66089
Saurabh's Mac <saurabh.sonawane1@gmail.com>
2024-03-15 05:59:06 -0400

    flaskappremaining

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -2,6 +2,7 @@ import sys
 import os
 from dataclasses import dataclass
 from pathlib import Path
+import numpy as np
 sys.path.append(str(Path(__file__).parent.parent.parent)) 
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier, GradientBoostingClassifier
@@ -44,8 +45,48 @@ class ModelTrainer:
                 "XGBoost":XGBClassifier()
             }
 
+            parameters = {
+            
+                "Decision Tree":
+                {'max_depth' : [3,5,7,9,10,15,20,25],
+                 'criterion' : ['gini', 'entropy'],
+                 'max_features' : ['sqrt', 'log2'],
+                'min_samples_split' : [2,4,6]
+             },
+                "Random Forest":
+                { "n_estimators":[int(x) for x in np.linspace(start = 10, stop = 80, num = 10)],
+                "max_features":['sqrt'],
+                "max_depth":[2,4,5],
+                "min_samples_split":[2, 5,7],
+                "min_samples_leaf":[1, 2,5],
+                "bootstrap":[True, False]
+             }, 
+                "Gradient Boosting": 
+                { 'learning_rate': [0.01, 0.1, 0.2],
+                 'subsample': [0.5, 0.75],
+                 'criterion': ['friedman_mse'],
+                 'min_samples_split': [2, 4, 8],
+                 'min_samples_leaf': [1, 2, 3],
+                 'max_depth': [3]
+             },
+                "AdaBoost": {
+                 'n_estimators': [50, 100, 200],
+                 'learning_rate': [0.01, 0.1, 1.0],
+                 'algorithm': ['SAMME', 'SAMME.R']
+             },
+                "XGBoost": 
+                {
+                 'learning_rate': [0.01, 0.1, 0.2],
+                'max_depth': [3, 4],
+                'min_child_weight': [1, 3],
+                 'subsample': [0.5],
+                'colsample_bytree': [0.5, 0.7]
+                }
+            
+            }
+
             model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
-                                             models=models)
+                                             models=models,param=parameters)
             
             best_model_score = max(sorted(model_report.values()))
             best_model_name = list(model_report.keys())[
@@ -63,7 +104,7 @@ class ModelTrainer:
             )
 
             predicted=best_model.predict(X_test)
-
+            print(best_model_name)
             accuracy = accuracy_score(y_test, predicted)
             return accuracy
         
